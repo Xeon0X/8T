1fc46c2613274485a766868a7e434f31549ddbc2
Xeon0X <xeon0x.gaming@gmail.com>
2024-03-03 12:10:27 +0100

    Fix build and bin folder for cmake

--- build/CMakeFiles/console.dir/src/console/Console.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/console.dir/src/console/Console.cpp.o: \
- /home/xeon0x/Nextcloud/Projets/8T/src/console/Console.cpp \
- /usr/include/stdc-predef.h \
- /home/xeon0x/Nextcloud/Projets/8T/src/console/Console.h
