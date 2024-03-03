4ab40a7ca58bcfb24970da1a318efd271654141e
Xeon0X <xeon0x.gaming@gmail.com>
2024-03-03 01:38:21 +0100

    CMake setup with ftxui lib

---
+++ build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
@@ -0,0 +1,4 @@
+_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
+ /home/xeon0x/Nextcloud/Projets/8T/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
+ /usr/include/stdc-predef.h \
+ /home/xeon0x/Nextcloud/Projets/8T/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
