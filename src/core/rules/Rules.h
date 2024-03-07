12c983bc85c143c794e75e10f9ba2f0b8a21dc69
Eclairsombre <alexandre.thny@gmail.com>
2024-03-07 11:55:07 +0100

    Add 'rules' directory to INPUT in Doxyfile

--- src/core/rules/Rules.h
+++ src/core/rules/Rules.h
@@ -1,15 +1,38 @@
 #ifndef RULES
 #define RULES
 
+/**
+ * @class Rules
+ * @brief This class is used to represent the rules of the game
+ */
 class Rules
 {
 private:
-    void *allCard[5];
+    void *allCard[5]; /**< All the cards of the game */
 
 public:
+    /**
+     * @brief Construct a new Rules object
+     *
+     */
     Rules(/* args */);
+    /**
+     * @brief Destroy the Rules object
+     *
+     */
     ~Rules();
+
+    /**
+     * @brief Get all the cards of the game
+     *
+     * @return void**
+     */
     void **getAllCard();
+
+    /**
+     * @brief Set all the cards of the game
+     *
+     */
     void setAllCard();
 };
 
